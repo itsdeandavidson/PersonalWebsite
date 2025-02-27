23f32bc74e68a6cfc7d70168322b60a49b001ce7
itsdeandavidson <72446907+itsdeandavidson@users.noreply.github.com>
2025-02-27 17:35:04 +0000

    pushing website files to github from gitea server

---
+++ Client/Classes/Theme.cs
@@ -0,0 +1,23 @@
+﻿using MudBlazor;
+
+namespace Client.Classes;
+
+public class Themes
+{
+	public static readonly MudTheme MyTheme = new()
+	{
+		PaletteLight = new PaletteLight()
+		{
+			Black = "rgba(10, 10, 10, 1)",
+			White = "rgba(248, 249, 250, 1)",
+			Background = "rgba(10, 10, 10, 1)",
+			DrawerBackground = "rgba(37, 36, 34, 1)",
+			DrawerText = "rgba(248, 249, 250, 1)",
+			DrawerIcon = "rgba(248, 249, 250, 1)",
+			AppbarBackground = "rgba(10, 10, 10, 1)",
+			AppbarText = "rgba(248, 249, 250, 1)",
+			TextPrimary = "rgba(248, 249, 250, 1)",
+			Dark = "rgba(10, 10, 10, 1)"
+		}
+	};
+}
---
+++ Client/Program.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.HttpOverrides;
+using MudBlazor.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+
+builder.Services.AddMudServices();
+
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+	app.UseExceptionHandler("/Error");
+	app.UseHsts();
+}
+
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+	ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+});
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
